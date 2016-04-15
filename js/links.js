32d8dfea4634472ac96b9669f806161b3db76f93
Snowyblack58 <davidzhao058@gmail.com>
2016-04-14 20:02:20 -0400

    Update for April 14

--- js/links.js
+++ js/links.js
@@ -1,18 +1,9 @@
-$("#project-cubing").click(function(){
-	window.open("http://tjcubing.org","_blank");
-});
-
-$("#project-binger").click(function(){
-	window.open("http://naitian.github.io/Binger/login.html","_blank");
-});
-
-$("#project-cookieclicker").click(function(){
-	window.open("https://www.tjhsst.edu/~2016dzhao/SideProjects/CookieCoder/","_blank");
-});
-
-$("#project-gis").click(function(){
-	window.open("https://www.tjhsst.edu/~2016dzhao/SideProjects/GIS/","_blank");
-});
+$("#project-anacostiaparkapp").click(function(){window.open("https://www.nps.gov/anac/index.htm","_blank");});
+$("#project-cubing").click(function(){window.open("http://tjcubing.org","_blank");});
+$("#project-fourier").click(function(){window.open("https://github.com/Snowyblack58/FourierTransform","_blank");});
+$("#project-binger").click(function(){window.open("http://naitian.github.io/Binger/login.html","_blank");});
+$("#project-cookieclicker").click(function(){window.open("https://www.tjhsst.edu/~2016dzhao/SideProjects/CookieCoder/","_blank");});
+$("#project-gis").click(function(){window.open("https://www.tjhsst.edu/~2016dzhao/SideProjects/GIS/","_blank");});
 
 //Contact Links
 
