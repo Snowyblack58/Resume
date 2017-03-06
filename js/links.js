ba0c52a13db0711d0f579d48819e1947b2e0e0d6
Snowyblack58 <davidzhao058@gmail.com>
2017-03-05 22:21:36 -0500

    Add CSO project

--- js/links.js
+++ js/links.js
@@ -1,18 +1,19 @@
-$("#project-anacostiaparkapp").click(function(){window.open("https://github.com/Snowyblack58/Anacostia-Park-App/","_blank");});
+$("#project-cso").click(function(){window.open("https://github.com/davidbzhao/CSO","_blank");});
+$("#project-anacostiaparkapp").click(function(){window.open("https://github.com/davidbzhao/Anacostia-Park-App/","_blank");});
 $("#project-botjs2").click(function(){window.open("https://github.com/naitian/botjs2","_blank");});
-$("#project-cubing").click(function(){window.open("http://tjcubing.org","_blank");});
-$("#project-fourier").click(function(){window.open("https://github.com/Snowyblack58/FourierTransform","_blank");});
+$("#project-cubing").click(function(){window.open("http://davidbzhao.github.io/TJCubing/","_blank");});
+//$("#project-fourier").click(function(){window.open("https://github.com/davidbzhao/FourierTransform","_blank");});
 $("#project-binger").click(function(){window.open("http://naitian.github.io/Binger/login.html","_blank");});
-$("#project-cookieclicker").click(function(){window.open("https://www.tjhsst.edu/~2016dzhao/SideProjects/CookieCoder/","_blank");});
-$("#project-gis").click(function(){window.open("https://www.tjhsst.edu/~2016dzhao/SideProjects/GIS/","_blank");});
+$("#project-cookieclicker").click(function(){window.open("http://naitian.github.io/CookieCoder/","_blank");});
+//$("#project-gis").click(function(){window.open("https://www.tjhsst.edu/~2016dzhao/SideProjects/GIS/","_blank");});
 
 //Contact Links
 
 $(".icon-github").click(function(){
-	window.open("https://github.com/Snowyblack58","_blank");
+	window.open("https://github.com/davidbzhao","_blank");
 });
 
 $(".icon-linkedin").click(function(){
-	window.open("https://www.linkedin.com/in/davidzhao058","_blank");
+	window.open("https://www.linkedin.com/in/davidbzhao","_blank");
 });
 
