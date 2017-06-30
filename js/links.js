b4c96d3943126939c2056d110aa4fc600a098cfe
David Zhao <davidzhao058@gmail.com>
2017-06-29 23:47:10 -0400

    Add functionality to project links

--- js/links.js
+++ js/links.js
@@ -1,12 +1,8 @@
-$("#project-gonad").click(function(){window.open("https://docs.google.com/spreadsheets/d/1eodouvW5og_1Cx7edDbODiB06yO4zs8mqVRoF-y54FQ/edit?usp=sharing","_blank");});
-$("#project-cso").click(function(){window.open("https://github.com/davidbzhao/CSO","_blank");});
-$("#project-anacostiaparkapp").click(function(){window.open("https://github.com/davidbzhao/Anacostia-Park-App/","_blank");});
-$("#project-botjs2").click(function(){window.open("https://github.com/naitian/botjs2","_blank");});
-$("#project-cubing").click(function(){window.open("http://davidbzhao.github.io/TJCubing/","_blank");});
-//$("#project-fourier").click(function(){window.open("https://github.com/davidbzhao/FourierTransform","_blank");});
-$("#project-binger").click(function(){window.open("http://naitian.github.io/Binger/login.html","_blank");});
-$("#project-cookieclicker").click(function(){window.open("http://naitian.github.io/CookieCoder/","_blank");});
-//$("#project-gis").click(function(){window.open("https://www.tjhsst.edu/~2016dzhao/SideProjects/GIS/","_blank");});
+// Project links
+$('.project-card').click(function(event){
+	var link = $(this).parent().attr('data-href');
+	window.open(link, '_blank');
+})
 
 //Contact Links
 
