418e1ffcf976466411ebc546b46b9b0d6590ae21
David Zhao <davidzhao058@gmail.com>
2017-07-19 21:48:06 -0400

    Move all experiences to partials

--- js/experiences.js
+++ js/experiences.js
@@ -2,10 +2,14 @@ define([
     'jquery',
     'underscore',
     'text!../../json/education.json',
+    'text!../../json/work.json',
+    'text!../../json/extracurricular.json',
     'text!../../partials/experience.html'
-], function($, _, educationText, Experience){
+], function($, _, educationText, workText, extracurricularText, Experience){
     $(document).ready(function(){
         loadEducationExperiences();
+        loadWorkExperiences();
+        loadExtracurricularExperiences();
         $(window).scrollTop(sessionStorage.getItem("preRefreshPosition"));
         initCheckpoints();
     })
@@ -20,6 +24,24 @@ define([
         for(var cnt = 0; cnt < educationJSON.length; cnt++){
             var newExperienceHTML = newExperienceTemplate(educationJSON[cnt]);
             $('#education').append(newExperienceHTML);
+        }
+    }
+
+    function loadWorkExperiences(){
+        var workJSON = JSON.parse(workText);
+        var newExperienceTemplate = _.template(Experience);
+        for(var cnt = 0; cnt < workJSON.length; cnt++){
+            var newExperienceHTML = newExperienceTemplate(workJSON[cnt]);
+            $('#work').append(newExperienceHTML);
+        }
+    }
+
+    function loadExtracurricularExperiences(){
+        var extracurricularJSON = JSON.parse(extracurricularText);
+        var newExperienceTemplate = _.template(Experience);
+        for(var cnt = 0; cnt < extracurricularJSON.length; cnt++){
+            var newExperienceHTML = newExperienceTemplate(extracurricularJSON[cnt]);
+            $('#extracurricular').append(newExperienceHTML);
         }
     }
 });
