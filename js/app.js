e25f185afd7dfb879d32eeef063708264ca900d2
David Zhao <davidzhao058@gmail.com>
2017-07-19 20:29:42 -0400

    Restructure to allow for underscore templates

---
+++ js/app.js
@@ -0,0 +1,11 @@
+require.config({
+    baseUrl: 'js',
+    paths: {
+        jquery: 'libs/jquery-1.11.3.min',
+        underscore: 'libs/underscore.min',
+        bootstrap: 'libs/bootstrap.min',
+        text: 'libs/text'
+    }
+})
+
+require(['js/experiences.js'])
---
+++ js/experiences.js
@@ -0,0 +1,7 @@
+define([
+    'jquery',
+    'underscore',
+    'text!../../partials/experience.html'
+], function($, _, Experience){
+    console.log(Experience);
+});
