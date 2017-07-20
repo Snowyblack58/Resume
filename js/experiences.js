236a388c2a9ab31cd54da1b78ec881eadc5d9d99
David Zhao <davidzhao058@gmail.com>
2017-07-19 21:03:33 -0400

    Create and append experience html from JSON

--- js/experiences.js
+++ js/experiences.js
@@ -3,8 +3,17 @@ define([
     'underscore',
     'text!../../json/education.json',
     'text!../../partials/experience.html'
-], function($, _, educationJSON, Experience){
-    console.log(educationJSON);
-    console.log(typeof(educationJSON));
-    var newExperienceTemplate = _.template(Experience);
+], function($, _, educationText, Experience){
+    $(document).ready(function(){
+        loadEducationExperiences();
+    })
+
+    function loadEducationExperiences(){
+        var educationJSON = JSON.parse(educationText);
+        var newExperienceTemplate = _.template(Experience);
+        for(var cnt = 0; cnt < educationJSON.length; cnt++){
+            var newExperienceHTML = newExperienceTemplate(educationJSON[cnt]);
+            $('#education').append(newExperienceHTML);
+        }
+    }
 });
